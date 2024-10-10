957a64b68e637ae9846e5a932d8be0ccb1918d52
birdenly <jomacaldas@hotmail.com.br>
2024-10-10 18:55:05 -0300

    DB implemented and Department created

---
+++ Jdbc/src/application/App.java
@@ -0,0 +1,10 @@
+package application;
+
+import model.entities.Department;
+
+public class App {
+    public static void main(String[] args) {
+        Department obj = new Department(1,"books");
+        System.out.println(obj);
+    }
+}
