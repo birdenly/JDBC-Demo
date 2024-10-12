1d6c4579f716b1c893c99f54d74b101e564b8316
birdenly <jomacaldas@hotmail.com.br>
2024-10-12 20:41:28 -0300

    findById implemented

--- Jdbc/src/application/App.java
+++ Jdbc/src/application/App.java
@@ -1,19 +1,14 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) {
-        Department obj = new Department(1,"books");
-        
-        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
-
         SellerDao sellerDao = DaoFactory.creatSellerDao();
+        
+        Seller seller = sellerDao.findById(3);
         
         System.out.println(seller);
     }
