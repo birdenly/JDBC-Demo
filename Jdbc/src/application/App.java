86ca487180fc80ba40222964ca98714b7a20f1ff
birdenly <jomacaldas@hotmail.com.br>
2024-10-12 21:33:53 -0300

    findByDepartment implamented

--- Jdbc/src/application/App.java
+++ Jdbc/src/application/App.java
@@ -1,15 +1,27 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
-
 public class App {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.creatSellerDao();
         
+        System.out.println("##### FIND BY ID #####");
         Seller seller = sellerDao.findById(3);
         
         System.out.println(seller);
+
+        System.out.println("\n##### FIND BY DEPARTMENT #####");
+
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+
+        for(Seller obj : list){
+            System.out.println(obj);
+        }
     }
 }
